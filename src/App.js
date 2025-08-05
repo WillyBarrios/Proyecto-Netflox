51e4f0b0a8f745ef75c92b7b24d413437fe47e7e
NickP41N <enriquevelassquez@gmail.com>
2025-08-04 18:48:29 -0600

    se agregó Footer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./Footer";
+import './App.css';
+
+const App = () => (
+    <div className="App">
+        <h3>Header</h3>
+        <h2>Header</h2>
+        <Footer />
+    </div>
+);
+
+export default App;
